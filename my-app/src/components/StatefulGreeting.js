83d5ada3af8fc5b7dbfb7d4cd647ce33e12143df
Kyle Mardell <kyleslufc@gmail.com>
2024-11-12 20:02:06 +0000

    Add code to set the state correctly using the 3 types of set state function.

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -7,14 +7,34 @@ class StatefulGreeting extends React.Component {
         this.state = {
             introduction: "Hello!",
             buttonText: "Exit",
+            count: 0,
         };
     }
 
     handleClick() {
         console.log("Button clicked.")
-        this.setState({
-            introduction: "Goodbye!",
-            buttonText: "Enter",
+        this.setState((prevState, prevProps) => {
+            console.log("Previous state: ", prevState);
+            console.log("Previous Props: ", prevProps);
+            return {
+                introduction: prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+                buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+            }   
+        }, () => {
+            // this callback executes AFTER the setState async function has finished
+            // add the optional callback as a second parameter to the setState function
+            console.log("New state: ", this.state.introduction);
+            console.log("New state: ", this.state.buttonText);
+        });
+    }
+
+    increment() {
+        this.setState((prevState, prevProps) => {
+            console.log("Previous state: ", prevState);
+            console.log("Previous Props: ", prevProps);
+            return {
+                count: prevState.count + 1,
+            }
         });
     }
 
@@ -23,6 +43,7 @@ class StatefulGreeting extends React.Component {
             <div>
                 <h1>{this.state.introduction} {this.props.greeting}</h1>
                 <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.increment()}>{this.state.count}</button>
             </div>
         )
     }
