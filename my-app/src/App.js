64e07837ace8ed60ff03d1b0bd0b65ee2434b21c
Kyle Mardell <kyleslufc@gmail.com>
2024-11-22 16:05:28 +0000

    Add a component that uses axios to send a get request to an api and render the response.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from './components/StatefulGreeting';
+import HTTPRequests from './components/HTTPRequests';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
-      <FunctionalGreetingWithProps greeting="nice to meet you!!" name="Mike" age="32" />
-      <StatefulGreeting greeting="I'm a stateful class component!" />
+      <HTTPRequests />
     </div>
   );
 }
